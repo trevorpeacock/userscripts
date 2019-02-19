010bc91d059ab0a50bbfddd61a154cc62d79e2e2
Samuel Walladge <samuel@swalladge.id.au>
2019-02-19 20:42:39 +1030

    push slack script

---
+++ slack-redirect.user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name        slack-direct
+// @namespace   swalladge.id.au
+// @description Bypass slack-redir
+// @include     https://*.slack.com/**
+// @version     1.1.0
+// @author      Samuel Walladge <samuel@swalladge.id.au> (https://swalladge.id.au)
+// @grant       none
+// ==/UserScript==
+
+function removeRedirects() {
+    let links = document.querySelectorAll('a[href^="https://slack-redir.net"]');
+    for (link of links) {
+      link.removeAttribute('onclick');
+      link.href = decodeURIComponent(link.href.slice(33)).replace(new RegExp("&v=3$"), "");
+      let thing = link.cloneNode();
+      while (link.firstChild) {
+        thing.appendChild(link.lastChild);
+      }
+      link.parentNode.replaceChild(thing, link);
+    }
+}
+
+setInterval(removeRedirects, 500);
