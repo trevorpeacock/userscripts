5eab115226acf1408dbf59089f2471ff6a59d1f7
Samuel Walladge <samuel@swalladge.id.au>
2019-02-19 20:54:26 +1030

    add s-to-search support for auslan.org.au

--- s-to-search.user.js
+++ s-to-search.user.js
@@ -5,7 +5,7 @@
 // @match *://www.wordreference.com/*
 // @match *://*.wiktionary.org/*
 // @grant none
-// @version     1.0.0
+// @version     1.1.0
 // @author      Samuel Walladge <samuel@swalladge.id.au> (https://swalladge.id.au)
 // ==/UserScript==
 
@@ -13,31 +13,40 @@
 document.addEventListener("keydown", evt => {
 
   if (window.location.hostname === "www.wordreference.com") {
+
     let s = document.getElementById("si");
     if (document.activeElement.tagName === "INPUT" || document.activeElement.tagName === "TEXTAREA") {
       return; // abort; an input element already focussed
     }
+    checkAndFocus(s, evt);
 
-    // check if 's' was pressed and focus
-    evt = evt || window.event;
-    if (evt.ctrlKey === false && evt.keyCode === 83) {
-      s.focus();
-      // avoid 's' being then typed into the input
-      evt.preventDefault();
-    }
   } else if (/.*\.wiktionary.org/.test(window.location.hostname)) {
 
     let s = document.getElementById("searchInput");
     if (document.activeElement.tagName === "INPUT" || document.activeElement.tagName === "TEXTAREA") {
       return; // abort; an input element already focussed
     }
+    checkAndFocus(s, evt);
 
-    // check if 's' was pressed and focus
+  } else if (window.location.hostname === "www.auslan.org.au") {
+
+    let s = document.getElementById("id_menu_query");
+    if (document.activeElement.tagName === "INPUT" || document.activeElement.tagName === "TEXTAREA") {
+      return; // abort; an input element already focussed
+    }
+    checkAndFocus(s, evt);
+
+  }
+
+});
+
+
+let checkAndFocus = (el, evt) => {
+    // check if 'el' was pressed and focus
     evt = evt || window.event;
     if (evt.ctrlKey === false && evt.keyCode === 83) {
-      s.focus();
+      el.focus();
       // avoid 's' being then typed into the input
       evt.preventDefault();
     }
-  }
-});
+}
