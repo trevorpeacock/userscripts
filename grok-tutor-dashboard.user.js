94f1d2beed2a7cf96a36be44666216bc069a0472
Samuel Walladge <samuel@swalladge.net>
2019-08-20 13:45:06 +0930

    consolidate userscripts

--- grok-tutor-dashboard.user.js
+++ grok-tutor-dashboard.user.js
@@ -2,9 +2,11 @@
 // @name        NCSS Tutor Dashboard Tools
 // @namespace   swalladge.net
 // @icon        https://static.groklearning-cdn.com/static/images/favicon.png
-// @description tutor dashboard tools enhancements
+// @description Tutor tools enhancements
 // @include     https://groklearning.com/tutor-messaging/*
-// @version     1.2.0
+// @include     https://groklearning.com/learn/*
+// @include     https://groklearning.com/view-submissions/*
+// @version     2.0.0
 // @grant       none
 // @author      Samuel Walladge <samuel@swalladge.net>
 // ==/UserScript==
@@ -13,72 +15,108 @@
 // this function injected into the page so it runs in the correct context
 function main() {
   window.addEventListener('load', () => {
+    if (/^\/tutor-messaging\/.*/.test(window.location.pathname)) {
+      // add button to turn *off* auto-assign for all
+      let btn2 = $('<a class="add-new-preference-btn"><span class="icon icon-bell2"></span></a>');
+      $('.component-container.tutor-preferences > h4').append(btn2);
+      btn2.click(() => {
+        $("a:contains('Disable auto-assign mode')").each(function() { this.click(); });
+      });
 
-    // add button to turn *off* auto-assign for all
-    let btn2 = $('<a class="add-new-preference-btn"><span class="icon icon-bell2"></span></a>');
-    $('.component-container.tutor-preferences > h4').append(btn2);
-    btn2.click(() => {
-      $("a:contains('Disable auto-assign mode')").each(function() { this.click(); });
-    });
+      // add button to turn on auto-assign for all
+      let btn = $('<a class="add-new-preference-btn"><span class="icon icon-bell3"></span></a>');
+      $('.component-container.tutor-preferences > h4').append(btn);
+      btn.click(() => {
+        $("a:contains('Enable auto-assign mode')").each(function() { this.click(); });
+      });
 
-    // add button to turn on auto-assign for all
-    let btn = $('<a class="add-new-preference-btn"><span class="icon icon-bell3"></span></a>');
-    $('.component-container.tutor-preferences > h4').append(btn);
-    btn.click(() => {
-      $("a:contains('Enable auto-assign mode')").each(function() { this.click(); });
-    });
+      // create a custom box and add custom buttons to that (so that dynamic loading doesn't clobber changes we make)
+      let customBox = $('<div class="component-container"><h4>Custom Controls</h4></div>');
+      $("div.component-container.tutor-preferences").after(customBox);
 
-    // create a custom box and add custom buttons to that (so that dynamic loading doesn't clobber changes we make)
-    let customBox = $('<div class="component-container"><h4>Custom Controls</h4></div>');
-    $("div.component-container.tutor-preferences").after(customBox);
-
-    // show/hide passed threads in replied or closed section
-    customBox.append($('<h5>My Threads / Replied or closed</h5>'));
-    let hidePassedBtn = $('<a class="btn btn-default btn-sm thread-action">Hide passed</a>');
-    customBox.append(hidePassedBtn);
-    hidePassedBtn.click(() => {
-      $(".thread-group > h4:contains('Replied or closed')").parent().find(".passed").parent().parent().hide();
-    });
-    let showPassedBtn = $('<a class="btn btn-default btn-sm thread-action">Show passed</a>');
-    customBox.append(showPassedBtn);
-    showPassedBtn.click(() => {
-      $(".thread-group > h4:contains('Replied or closed')").parent().find(".passed").parent().parent().show();
-    });
+      // show/hide passed threads in replied or closed section
+      customBox.append($('<h5>My Threads / Replied or closed</h5>'));
+      let hidePassedBtn = $('<a class="btn btn-default btn-sm thread-action">Hide passed</a>');
+      customBox.append(hidePassedBtn);
+      hidePassedBtn.click(() => {
+        $(".thread-group > h4:contains('Replied or closed')").parent().find(".passed").parent().parent().hide();
+      });
+      let showPassedBtn = $('<a class="btn btn-default btn-sm thread-action">Show passed</a>');
+      customBox.append(showPassedBtn);
+      showPassedBtn.click(() => {
+        $(".thread-group > h4:contains('Replied or closed')").parent().find(".passed").parent().parent().show();
+      });
 
 
-    // desktop notifications (based on script by @forkbomb)
-    Notification.requestPermission();
+      // desktop notifications (based on script by @forkbomb)
+      Notification.requestPermission();
 
-    Handlebars.templates['__chatThreadAssignment'] = Handlebars.templates['chat-thread-assignment-notification-popup'];
-    Handlebars.templates['chat-thread-assignment-notification-popup'] = function() {
-      new Notification(
-        "Grok: new chat thread offer!",
-        {
-          'icon': 'https://static.groklearning-cdn.com/static/images/favicon.png'
-        });
-      return Handlebars.templates['__chatThreadAssignment'].apply(this, arguments);
-    };
+      Handlebars.templates['__chatThreadAssignment'] = Handlebars.templates['chat-thread-assignment-notification-popup'];
+      Handlebars.templates['chat-thread-assignment-notification-popup'] = function() {
+        new Notification(
+          "Grok: new chat thread offer!",
+          {
+            'icon': 'https://static.groklearning-cdn.com/static/images/favicon.png'
+          });
+        return Handlebars.templates['__chatThreadAssignment'].apply(this, arguments);
+      };
 
-    Handlebars.templates['__chatThread'] = Handlebars.templates['chat-thread-notification-popup'];
-    Handlebars.templates['chat-thread-notification-popup'] = function() {
-      new Notification(
-        "Grok: chat thread notification!",
-        {
-          'icon': 'https://static.groklearning-cdn.com/static/images/favicon.png'
-        });
-      return Handlebars.templates['__chatThread'].apply(this, arguments);
-    };
+      Handlebars.templates['__chatThread'] = Handlebars.templates['chat-thread-notification-popup'];
+      Handlebars.templates['chat-thread-notification-popup'] = function() {
+        new Notification(
+          "Grok: chat thread notification!",
+          {
+            'icon': 'https://static.groklearning-cdn.com/static/images/favicon.png'
+          });
+        return Handlebars.templates['__chatThread'].apply(this, arguments);
+      };
 
-    Handlebars.templates['__chatThreadState'] = Handlebars.templates['chat-thread-state-change-notification-popup'];
-    Handlebars.templates['chat-thread-state-change-notification-popup'] = function() {
-      new Notification(
-        "Grok: chat thread state change!",
-        {
-          'icon': 'https://static.groklearning-cdn.com/static/images/favicon.png'
-        });
-      return Handlebars.templates['__chatThreadState'].apply(this, arguments);
-    };
+      Handlebars.templates['__chatThreadState'] = Handlebars.templates['chat-thread-state-change-notification-popup'];
+      Handlebars.templates['chat-thread-state-change-notification-popup'] = function() {
+        new Notification(
+          "Grok: chat thread state change!",
+          {
+            'icon': 'https://static.groklearning-cdn.com/static/images/favicon.png'
+          });
+        return Handlebars.templates['__chatThreadState'].apply(this, arguments);
+      };
 
+    } else if (/^\/learn\/.*/.test(window.location.pathname)) {
+      // Adds a button to copy the slide url as a markdown link.
+      let copySlide = () => {
+        let url = window.location.href;
+        let text = '[this slide](' + url + ')';
+        navigator.clipboard.writeText(text);
+      };
+      let copyBtn = $('<a class="action"><span class="icon icon-copy"></span><span class="title">Copy mkd link</span></a>');
+      $('#action-bar-menu-left').append(copyBtn);
+      copyBtn.click(copySlide);
+    } else if (/^\/view-submissions\/.*/.test(window.location.pathname)) {
+
+      // Adds a button to hide PII in the rare case when you need to get a
+      // screenshot.
+      let hidePII = () => {
+        $("div.chat-post.student span.name").html("Student Name");
+        $("div.chat-post.student img.avatar").attr("src", "https://www.gravatar.com/avatar/55502f40dc8b7c769880b10874abc9d0?s=64&r=g&d=retro");
+        $(".tutoring-user-details").html("<strong>Student Name</strong> (Student at Placeholder School)");
+      };
+
+      let btn = $('<a class="action"><span class="icon icon-user"></span><span class="title">Hide PII</span></a>');
+      $('#action-bar-menu-left').append(btn);
+      btn.click(hidePII);
+
+      // Adds a button to copy the slide url as a markdown link.
+      let copySlide = () => {
+        let link = $("a:contains('View slide in course')");
+        if (link.length !== 1) { return; }
+        let url = link[0].href;
+        let text = '[this slide](' + url + ')';
+        navigator.clipboard.writeText(text);
+      };
+      let copyBtn = $('<a class="action"><span class="icon icon-copy"></span><span class="title">Copy mkd link</span></a>');
+      $('#action-bar-menu-left').append(copyBtn);
+      copyBtn.click(copySlide);
+    }
   });
 }
 
