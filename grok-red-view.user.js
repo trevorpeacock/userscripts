47a1ce29cb5a8777a9790b55153c541adfc5bbf2
Samuel Walladge <samuel@swalladge.net>
2019-08-20 09:30:35 +0930

    add copy markdown link btn

--- grok-red-view.user.js
+++ grok-red-view.user.js
@@ -4,7 +4,7 @@
 // @icon        https://static.groklearning-cdn.com/static/images/favicon.png
 // @description tutor red view enhancements
 // @include     https://groklearning.com/view-submissions/*
-// @version     1.0.0
+// @version     1.1.0
 // @grant       none
 // @author      Samuel Walladge <samuel@swalladge.net>
 // ==/UserScript==
@@ -21,14 +21,29 @@ function main() {
     $(".tutoring-user-details").html("<strong>Student Name</strong> (Student at Placeholder School)");
   };
 
-  let btn = $('<a class="action"><span class="icon icon-question4"></span><span class="title">Hide PII</span></a>');
+  let btn = $('<a class="action"><span class="icon icon-user"></span><span class="title">Hide PII</span></a>');
   $('#action-bar-menu-left').append(btn);
   btn.click(hidePII);
+
+  // Adds a button to copy the slide url as a markdown link.
+  let copySlide = () => {
+    let link = $("a:contains('View slide in course')");
+    console.trace(link);
+    if (link.length !== 1) { return; }
+    let url = link[0].href;
+    let text = '[this slide](' + url + ')';
+    console.trace(text);
+    navigator.clipboard.writeText(text);
+  };
+  let copyBtn = $('<a class="action"><span class="icon icon-copy"></span><span class="title">Copy mkd link</span></a>');
+  $('#action-bar-menu-left').append(copyBtn);
+  copyBtn.click(copySlide);
 }
 
 
 var script = document.createElement('script');
 script.appendChild(document.createTextNode('('+ main +')();'));
 (document.body || document.head || document.documentElement).appendChild(script);
+
 
 
